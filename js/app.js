b0c5693b47ff551d4cf64ac7d0b150482b83ee12
Cicero Jonyedson <cicerojonyedison@gmail.com>
2022-10-24 17:11:04 -0300

    html e css prontos, fazer o arquivo de js

---
+++ js/app.js
@@ -0,0 +1,23 @@
+[
+  {
+    id: 1,
+    name: "Raquel Lira",
+    job: "Full Stack Developer",
+    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXMKvjoFY41ypWrRdVnut-cXUZPCAXcY18PA&usqp=CAU',
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore beatae debitis placeat ad tenetur sunt cupiditate quos, quas pariatur, modi velit mollitia error dignissimos nisi vero tempore blanditiis impedit distinctio.",
+  },
+  {
+    id: 2,
+    name: "Paulo Santos",
+    job: "Front-end Developer",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrRXwHqmljcF0xlUgcoqZXuN7d15HGEfUDISUKDqVnWBYoktjYNFdQicDy4E_oQiNMXyE&usqp=CAU",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore beatae debitis placeat ad tenetur sunt cupiditate quos, quas pariatur, modi velit mollitia error dignissimos nisi vero tempore blanditiis impedit distinctio.",
+  },
+  {
+    id: 3,
+    name: "Marcos Felipe",
+    job: "Back-end Developer",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFNkJpg5tIB3DZsMqxgGCyRtSwDuav9LEdbZI06evMasI6tmkPahgI1ftvuA7qbHSsbgg&usqp=CAU",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore beatae debitis placeat ad tenetur sunt cupiditate quos, quas pariatur, modi velit mollitia error dignissimos nisi vero tempore blanditiis impedit distinctio.",
+  },
+];
